e6ac871a4eb7e66dc16308c95eedb96bcc9a897b
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-05 23:42:25 -0500

    create template for readme

---
+++ src/template.js
@@ -0,0 +1,33 @@
+// create template literal
+module.exports = templateData => {
+    return `
+    ${generateBadges(licenses)}
+    # ${templateData.title}
+    ## Table of Contents
+    * [Description](#Description)
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    * [License](#License)
+    ## Description 
+    ${templateData.description}
+    ## Installation
+    ${templateData.installation}
+    ## Usage
+    ${templateData.usage}
+    ## Contributing
+    ${templateData.contributing}
+    ## Tests
+    ${templateData.tests}
+    ## Questions
+    For questions, please visit the github profile.
+    Github profile : https://github.com/${templateData.github}
+    Please feel free to contact in email.
+    Email: ${templateData.email}
+    ## License
+    ${generateLicense(licenses)}
+
+    `
+}
